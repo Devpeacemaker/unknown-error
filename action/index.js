4ac2578a790c4f38b7a40a0da0edf1e5bd09adab
Peacemaker <devpeacemd@gmail.com>
2025-07-31 09:52:22 +0300

    Update index.js

--- action/index.js
+++ action/index.js
@@ -264,7 +264,7 @@ try {
       console.log(color("Congrats, PEACE-HUB has successfully connected to this server", "green"));
       console.log(color("Follow me on Instagram as peacemaker_hunter72", "red"));
       console.log(color("Text the bot number with menu to check my command list"));
-      cconst Texxt = `🟢 𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙴𝙳  ╍>〚𝙿𝙴𝙰𝙲𝙴-𝙷𝚄𝙱〛\n`+`🎛️ 𝙼𝙾𝙳𝙴  ╍>〚${mode}〛\n`+`🔣 𝙿𝚁𝙴𝙵𝙸𝚇  ╍>〚 ${prefix} 〛`
+      const Text = `🟢 𝙲𝙾𝙽𝙽𝙴𝙲𝚃𝙴𝙳  ╍>〚𝙿𝙴𝙰𝙲𝙴-𝙷𝚄𝙱〛\n`+`🎛️ 𝙼𝙾𝙳𝙴  ╍>〚${mode}〛\n`+`🔣 𝙿𝚁𝙴𝙵𝙸𝚇  ╍>〚 ${prefix} 〛`
       client.sendMessage(client.user.id, { text: Texxt });
     }
   });
