80e9b94a9c3e27b72669e0c735879df71fad9558
Peacemaker <devpeacemd@gmail.com>
2025-08-06 21:37:17 +0300

    ᴘᴇᴀᴄᴇ ʜᴜʙ

--- peacemaker/peace.js
+++ peacemaker/peace.js
@@ -4696,7 +4696,7 @@ if (!text) return m.reply("𝗣𝗿𝗼𝘃𝗶𝗱𝗲 𝗮 𝘃𝗮𝗹𝗶�
     case "ping":
 case "speed": {
     await loading();
-    m.reply(`🔶 Peacehub Speed ${Rspeed.toFixed(1)}ms`);
+    m.reply(`🔶 Peacehub speed ${Rspeed.toFixed(4)}ms`);
 }
 break; 
 
